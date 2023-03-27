768f3b80ef7143d1891efbe51bd104f84d6ee958
Ayelsh <15768206596@163.com>
2023-03-27 16:17:57 +0800

    First init

---
+++ src/main/java/com/emergencyfood/PaimonTravelReservation/PaimonTravelReservationApplication.java
@@ -0,0 +1,13 @@
+package com.emergencyfood.PaimonTravelReservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaimonTravelReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaimonTravelReservationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emergencyfood/PaimonTravelReservation/PaimonTravelReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emergencyfood.PaimonTravelReservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaimonTravelReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
