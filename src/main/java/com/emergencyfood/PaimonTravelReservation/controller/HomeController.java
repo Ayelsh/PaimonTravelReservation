747310c99343f65b8bae83994dfdf98963bd2c8a
Ayelsh <15768206596@163.com>
2023-03-27 17:42:05 +0800

    First init

--- src/main/java/com/emergencyfood/PaimonTravelReservation/controller/HomeController.java
+++ src/main/java/com/emergencyfood/PaimonTravelReservation/controller/HomeController.java
@@ -1,13 +1,16 @@
 package com.emergencyfood.PaimonTravelReservation.controller;
 
+import io.swagger.annotations.Api;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@Api(tags = "主页")
 @Controller
 public class HomeController {
 
-    @RequestMapping(value = "/home")
+    @GetMapping(value = "/home")
     public @ResponseBody String home(){
         return "Hello world!";
 
